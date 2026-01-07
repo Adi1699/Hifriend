8d37ad100df305050fe96b519e896048ee653745
Aditya Sharma <45919676+Adi1699@users.noreply.github.com>
2026-01-07 13:14:10 +0530

    update the more files"

---
+++ testcase_scripts/PIM/Verify_the_functionality_of_toggling_the_Show_Deprecated_Fields_switch_in_PIM_Configuration_and_saving_the_changes.py
@@ -0,0 +1,47 @@
+import asyncio
+from playwright.async_api import async_playwright
+
+async def run():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        context = await browser.new_context()
+        page = await context.new_page()
+
+        await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        await page.wait_for_load_state(state='load')
+        element = page.locator('xpath=//input[@name="username" and @placeholder="Username" and contains(normalize-space(@class), "oxd-input")]').nth(0)
+        await element.fill('')
+        await element.type('Admin')
+        await page.wait_for_timeout(2000)
+        element = page.locator('xpath=//input[@name="password" and @placeholder="Password" and @type="password" and contains(normalize-space(@class), "oxd-input")]').nth(0)
+        await element.fill('')
+        await element.type('admin123')
+        await page.wait_for_timeout(2000)
+        element = page.locator('xpath=html/body/div/div/div/div/div/div[2]/div[2]/form/div[3]/button').nth(0)
+        await element.click()
+        await page.wait_for_timeout(2000)
+        await page.keyboard.press('PageDown')
+        await page.go_back()
+        await page.wait_for_timeout(2000)
+        await page.go_back()
+        await page.wait_for_timeout(2000)
+        await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewEmployeeList')
+        await page.wait_for_load_state(state='load')
+        element = page.locator('xpath=html/body/div/div/div[2]/div[2]/div/div/div/div[2]/div[3]/button').nth(0)
+        await element.click()
+        await page.wait_for_timeout(2000)
+        await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/pim/configurePim')
+        await page.wait_for_load_state(state='load')
+        element = page.locator('xpath=html/body/div/div/div[2]/div[2]/div/div/form/div/div/div/div/label').nth(0)
+        await element.click()
+        await page.wait_for_timeout(2000)
+        element = page.locator('xpath=html/body/div/div/div[2]/div[2]/div/div/form/div[3]/button').nth(0)
+        await element.click()
+        await page.wait_for_timeout(2000)
+        await page.keyboard.press('PageDown')
+        await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/pim/configurePim')
+        await page.wait_for_load_state(state='load')
+
+        await browser.close()
+
+asyncio.run(run())
