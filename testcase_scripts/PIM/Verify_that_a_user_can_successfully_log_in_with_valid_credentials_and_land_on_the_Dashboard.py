48046799319a580a0060b905fe16bdfe5a4883d8
Aditya Sharma <45919676+Adi1699@users.noreply.github.com>
2026-01-07 18:42:36 +0530

    update the more files"

---
+++ testcase_scripts/PIM/Verify_that_a_user_can_successfully_log_in_with_valid_credentials_and_land_on_the_Dashboard.py
@@ -0,0 +1,28 @@
+import asyncio
+from playwright.async_api import async_playwright, TimeoutError
+
+async def run():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        context = await browser.new_context()
+        page = await context.new_page()
+
+        await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login", wait_until='domcontentloaded')
+        try:
+            await page.wait_for_load_state('load', timeout=2000)
+        except TimeoutError:
+            pass
+
+        await page.get_by_role('textbox', name='Username').fill("Admin")
+
+        await page.get_by_role('textbox', name='Password').fill("admin123")
+
+        await page.get_by_role('button', name='Login').click()
+        try:
+            await page.wait_for_load_state('load', timeout=2000)
+        except TimeoutError:
+            pass
+
+        await browser.close()
+
+asyncio.run(run())
