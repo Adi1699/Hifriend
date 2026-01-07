5b3df7b9cd8396d73f04a774702d01267d4de0a5
Aditya Sharma <45919676+Adi1699@users.noreply.github.com>
2026-01-07 18:42:37 +0530

    update the more files"

---
+++ testcase_scripts/PIM/Verify_successful_navigation_to_the_PIM_-_Employee_Information_section_and_display_of_employee_records.py
@@ -0,0 +1,25 @@
+import asyncio
+from playwright.async_api import async_playwright
+
+async def run():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        context = await browser.new_context()
+        page = await context.new_page()
+
+        await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login", wait_until='domcontentloaded')
+        await page.wait_for_load_state('load', timeout=2000)
+
+        await page.get_by_role('textbox', name='Username').fill("Admin")
+
+        await page.get_by_role('textbox', name='Password').fill("admin123")
+
+        await page.get_by_role('button', name='Login').click()
+        await page.wait_for_load_state('load', timeout=2000)
+
+        await page.get_by_role('link', name='PIM').click()
+        await page.wait_for_load_state('load', timeout=2000)
+
+        await browser.close()
+
+asyncio.run(run())
